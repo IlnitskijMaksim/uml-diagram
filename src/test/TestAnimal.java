b43d4f316332a471a441a8bac5f8fafd8f1cf63e
Ільницький Максим <112692170+IlnitskijMaksim@users.noreply.github.com>
2023-02-24 22:44:48 +0200

    Update TestAnimal.java

--- src/test/TestAnimal.java
+++ src/test/TestAnimal.java
@@ -2,9 +2,21 @@ package test;
 
 import domain.Penguin;
 
+
+ /**
+ * The class Test animal
+ */ 
 public class TestAnimal {
 
-    public static void main(String[] args) {
+
+/** 
+ *
+ * Main
+ *
+ * @param args  the args
+ */
+    public static void main(String[] args) { 
+
         Penguin penguin = new Penguin("Tuxedo");
         System.out.println(penguin);
         penguin.swim();
